d00799e174681f1ba0ef7d59d1436c694f610406
Las <haho013039@gmail.com>
2021-04-03 23:43:23 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ server_docker_image_name=atmo-server-"$timestamp"
 echo "docker iamge name is " + "$server_docker_image_name"
 
 docker build -t "$server_docker_image_name" .
-docker run -d -p 8080:8080 "$server_docker_image_name"
+docker run -d -p 8081:8080 "$server_docker_image_name"
 echo "SERVER DEPLOY DONE"
 
 echo "FRONTEND DEPLOY START"
@@ -22,7 +22,7 @@ front_docker_image_name=atmo-front-"$timestamp"
 echo "docker iamge name is " + "$front_docker_image_name"
 
 docker build -t "$front_docker_image_name" ./frontend/weather/
-docker run -d -p 3000:8081 "$front_docker_image_name"
+docker run -d -p 8080:3000 "$front_docker_image_name"
 echo "FRONTEND DEPLOY DONE"
 
 echo "DEPLOY END"
